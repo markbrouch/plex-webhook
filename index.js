3ddfa12cddf26718a824c82f5cee700bf3ec673a
Mark Brouch <mbrouch@walmartlabs.com>
2017-04-30 00:37:44 -0500

    🛠 Create Lambda function

--- index.js
+++ index.js
@@ -1,29 +1,62 @@
-const express = require('express')
-const multer = require('multer')
+'use strict'
+const https = require('https')
+const Busboy = require('busboy')
 
-// const PLAYER_UUID = process.env.PLAYER_UUID
-// if (!PLAYER_UUID) throw new Error('No player uuid specified.')
+const readPayload = payload => {
+  const PLAY = 'media.play'
+  const PAUSE = 'media.pause'
+  const RESUME = 'media.resume'
+  const STOP = 'media.stop'
 
-const PLAY = 'media.play'
-const PAUSE = 'media.pause'
-const RESUME = 'media.resume'
-const STOP = 'media.stop'
+  const { event, Player, Metadata } = payload
 
-const upload = multer()
+  const options = {
+    hostname: 'www.meethue.com',
+    path: `/api/sendmessage?token=${process.env.HUE_TOKEN}`,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  }
 
-const app = express()
+  if (
+    process.env.PLAYER_UUID === Player.uuid &&
+    Metadata.type === 'movie' &&
+    (event === PLAY || event === STOP || event === PAUSE || event === RESUME)
+  ) {
+    const scene = event === PLAY || event === RESUME
+      ? process.env.HUE_SCENE_THEATER
+      : process.env.HUE_SCENE_DIMMED
 
-app.post('/', upload.single('thumb'), function(req, res, next) {
-  const { Player, event } = JSON.parse(req.body.payload)
+    const body = `clipmessage={ bridgeId: "${process.env.HUE_BRIDGE_ID}", clipCommand: { url: "/api/0/groups/${process.env.HUE_GROUP_ID}/action", method: "PUT", body: { scene: "${scene}" } } }`
+    const req = https.request(options)
 
-  // if (Player.uuid === PLAYER_UUID) {
-  if (event === PLAY) console.log('play')
-  if (event === PAUSE) console.log('pause')
-  if (event === RESUME) console.log('resume')
-  if (event === STOP) console.log('stop')
-  // }
+    req.write(body)
+    req.end()
+  }
+}
 
-  res.sendStatus(200)
-})
+exports.handler = (event, context, callback) => {
+  const headers = {}
+  Object.keys(event.headers).forEach(
+    key => (headers[key.toLowerCase()] = event.headers[key])
+  )
 
-app.listen(process.env.PORT || 8000)
+  const busboy = new Busboy({
+    headers: headers
+  })
+
+  busboy.on('field', (fieldname, value) => {
+    if (fieldname === 'payload') {
+      const payload = JSON.parse(value)
+
+      readPayload(payload)
+
+      callback(null, {
+        payload
+      })
+    }
+  })
+
+  busboy.write(Buffer.from(event.body, 'base64').toString('utf8'))
+}
