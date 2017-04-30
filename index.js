74f65c7b59174a9aad39f58e0ca750a492fb4bdf
Mark Brouch <mbrouch@walmartlabs.com>
2017-04-30 10:48:59 -0500

    🔧 Use semicolons 🔧 Use semicolons

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-'use strict'
-const https = require('https')
-const Busboy = require('busboy')
+'use strict';
+const https = require('https');
+const Busboy = require('busboy');
 
 const readPayload = payload => {
-  const PLAY = 'media.play'
-  const PAUSE = 'media.pause'
-  const RESUME = 'media.resume'
-  const STOP = 'media.stop'
+  const PLAY = 'media.play';
+  const PAUSE = 'media.pause';
+  const RESUME = 'media.resume';
+  const STOP = 'media.stop';
 
-  const { event, Player, Metadata } = payload
+  const { event, Player, Metadata } = payload;
 
   const options = {
     hostname: 'www.meethue.com',
@@ -17,7 +17,7 @@ const readPayload = payload => {
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
     }
-  }
+  };
 
   if (
     process.env.PLAYER_UUID === Player.uuid &&
@@ -26,37 +26,37 @@ const readPayload = payload => {
   ) {
     const scene = event === PLAY || event === RESUME
       ? process.env.HUE_SCENE_THEATER
-      : process.env.HUE_SCENE_DIMMED
+      : process.env.HUE_SCENE_DIMMED;
 
-    const body = `clipmessage={ bridgeId: "${process.env.HUE_BRIDGE_ID}", clipCommand: { url: "/api/0/groups/${process.env.HUE_GROUP_ID}/action", method: "PUT", body: { scene: "${scene}" } } }`
-    const req = https.request(options)
+    const body = `clipmessage={ bridgeId: "${process.env.HUE_BRIDGE_ID}", clipCommand: { url: "/api/0/groups/${process.env.HUE_GROUP_ID}/action", method: "PUT", body: { scene: "${scene}" } } }`;
+    const req = https.request(options);
 
-    req.write(body)
-    req.end()
+    req.write(body);
+    req.end();
   }
-}
+};
 
 exports.handler = (event, context, callback) => {
-  const headers = {}
+  const headers = {};
   Object.keys(event.headers).forEach(
     key => (headers[key.toLowerCase()] = event.headers[key])
-  )
+  );
 
   const busboy = new Busboy({
     headers: headers
-  })
+  });
 
   busboy.on('field', (fieldname, value) => {
     if (fieldname === 'payload') {
-      const payload = JSON.parse(value)
+      const payload = JSON.parse(value);
 
-      readPayload(payload)
+      readPayload(payload);
 
       callback(null, {
         payload
-      })
+      });
     }
-  })
+  });
 
-  busboy.write(Buffer.from(event.body, 'base64').toString('utf8'))
-}
+  busboy.write(Buffer.from(event.body, 'base64').toString('utf8'));
+};
